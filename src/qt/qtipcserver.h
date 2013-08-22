a94b706c9f68b8cd5ecde988c62786af4e1efa0c
adam <Orbitcoin@Orbitcoin.org>
2013-08-22 13:45:40 -0600

    all ur catz r belong to blehcoin

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define blehcoin-Qt message queue name
+#define blehcoinURI_QUEUE_NAME "blehcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
