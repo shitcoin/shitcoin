c39f13c6081f98a1604a2f6d86003bd43253d434
Fractal <fractal@fractured.org>
2014-01-10 14:26:34 -0800

    Missed one boundary case for blocks prior to checkpoint

--- src/main.cpp
+++ src/main.cpp
@@ -942,34 +942,39 @@ uint256 WantedByOrphan(const CBlock* pblockOrphan)
 
 long long int GetProofOfWorkReward(int nHeight, int64 nFees, uint256 prevHash)
 {
-    int64 nSubsidy = 500000000 * COIN;
-    int64 nMinimumsubsidy = 1000000 * COIN;
-    int64 nMinimumsubsidy2 = 3000000 * COIN;
-
-    // Subsidy is cut in half every 210000 blocks, which will occur approximately every 4 years
-    if (nHeight < 128000) {
-        nSubsidy >>= (nHeight / 2000);
-    } else {
-        nSubsidy = nMinimumsubsidy2;
-    }
-	if (nHeight < 30000 && nSubsidy < nMinimumsubsidy)
-	{ 
-	    nSubsidy = nMinimumsubsidy;
-    }
-    else if (nHeight > 30000 && nSubsidy < nMinimumsubsidy2)
-    {
-        nSubsidy = nMinimumsubsidy2;
-    }
-    else if (nHeight == 77777) 
-    {
-        nSubsidy = 1111177770000 * COIN;
-    }
-	else if (nSubsidy < nMinimumsubsidy)
-	{
-		nSubsidy = nMinimumsubsidy;
-	}
+	int64 nSubsidy = 500000000 * COIN;
+	int64 nMinimumsubsidy = 1000000 * COIN;
+	int64 nMinimumsubsidy2 = 3000000 * COIN;
 	
-    return nSubsidy + nFees;
+	// Subsidy is cut in half every 210000 blocks, which will occur approximately every 4 years
+	if (nHeight < 543000) {
+		// accept buggy work up to first checkpoing
+		nSubsidy >>= (nHeight / 2000);
+			if (nHeight < 30000 && nSubsidy < nMinimumsubsidy)
+			{ 
+				nSubsidy = nMinimumsubsidy;
+			}
+			else if (nHeight > 30000 && nSubsidy < nMinimumsubsidy2)
+			{
+				nSubsidy = nMinimumsubsidy2;
+			}
+			else if (nHeight > 75000) 
+			{
+				nSubsidy = 500000000 * COIN;
+			}
+			else if (nHeight == 77777) 
+			{
+				nSubsidy = 1111177770000 * COIN;
+			}
+			else if (nSubsidy < nMinimumsubsidy)
+			{
+				nSubsidy = nMinimumsubsidy;
+			}
+	} else {
+		nSubsidy = nMinimumsubsidy2;
+	}
+
+	return nSubsidy + nFees;
 }
 // miner's coin stake reward based on nBits and coin age spent (coin-days)
 int64 GetProofOfStakeReward(int64 nCoinAge, unsigned int nBits, unsigned int nTime)
